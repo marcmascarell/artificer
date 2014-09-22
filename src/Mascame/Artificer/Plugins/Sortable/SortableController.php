e68dd29c2a8f1ca1f002dd7139ce3d7f6597b304
marc <marcmascarell2gmail.com>
2014-09-22 16:20:13 +0200

    fix sortable on delete when no records

--- src/Mascame/Artificer/Plugins/Sortable/SortableController.php
+++ src/Mascame/Artificer/Plugins/Sortable/SortableController.php
@@ -27,27 +27,30 @@ class SortableController extends Artificer {
 		$this->new_id = $new_sort_id;
 
 		$item = $this->model->where('sort_id', '=', $this->old_id)->first();
-		$item->sort_id = 0;
 
-		$direction = ($old_sort_id < $new_sort_id) ? 'bigger' : 'smaller';
+		if (!empty($item)) {
+			$item->sort_id = 0;
 
-		if ($direction == 'bigger') {
-			while ($old_sort_id <= $new_sort_id) {
-				$new = $old_sort_id - 1;
-				$this->updateSort($old_sort_id, $new);
-				$old_sort_id ++;
+			$direction = ($old_sort_id < $new_sort_id) ? 'bigger' : 'smaller';
+
+			if ($direction == 'bigger') {
+				while ($old_sort_id <= $new_sort_id) {
+					$new = $old_sort_id - 1;
+					$this->updateSort($old_sort_id, $new);
+					$old_sort_id ++;
+				}
+			} else {
+				while ($old_sort_id >= $new_sort_id) {
+					$this->updateSort($old_sort_id, $old_sort_id + 1);
+					$old_sort_id --;
+				}
 			}
-		} else {
-			while ($old_sort_id >= $new_sort_id) {
-				$this->updateSort($old_sort_id, $old_sort_id + 1);
-				$old_sort_id --;
-			}
+
+			$item->sort_id = $this->new_id;
+			$item->save();
+
+			Notification::success('<b>Success!</b> The table has been reordered!', true);
 		}
-
-		$item->sort_id = $this->new_id;
-		$item->save();
-
-        Notification::success('<b>Success!</b> The table has been reordered!', true);
 
 		return Redirect::route('admin.all', array('slug' => $this->modelObject->getRouteName()));
 	}
