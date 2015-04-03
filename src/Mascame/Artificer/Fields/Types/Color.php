3c46ad04b41df88701c2981ff696f10e5859b8fb
Marc Mascarell <marcmascarell@gmail.com>
2015-04-03 17:57:47 +0200

    add color type

---
+++ src/Mascame/Artificer/Fields/Types/Color.php
@@ -0,0 +1,21 @@
+<?php namespace Mascame\Artificer\Fields\Types;
+
+use Mascame\Artificer\Fields\Field;
+use Form;
+use Mascame\ArtificerWidgets\Datepicker\Datepicker;
+
+class Color extends Field {
+
+	public function input()
+	{
+		?>
+		<div class="input-group">
+			<div class="input-group-addon">
+				<i class="fa fa-eyedropper"></i>
+			</div>
+			<?php print Form::input('color', $this->name, $this->value, $this->attributes->all()); ?>
+		</div>
+	<?php
+	}
+
+}
