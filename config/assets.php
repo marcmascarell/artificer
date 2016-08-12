5f59e793a9c74d8a9e3448f71c012b3a8d97cb01
marcmascarell <marcmascarell@gmail.com>
2016-08-12 18:32:25 +0200

    = require only what is used

--- config/assets.php
+++ config/assets.php
@@ -2,23 +2,14 @@
 
 return [
 
-	'collections' => [
+    'collections' => [
         // jQuery (CDN)
-        'jquery-cdn' => [
-            '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'
-        ],
-
-        // Twitter Bootstrap (CDN)
-        'bootstrap-cdn' => [
-            'jquery-cdn',
-            '//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
-            '//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css',
-            '//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'
-        ],
+        'jquery-cdn' => ['//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js'],
     ],
 
     'assets' => [
         'jquery-cdn',
         'packages/mascame/admin/js/core/restfulizer.js'
     ]
+
 ];
