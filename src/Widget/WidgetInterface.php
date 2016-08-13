1153e7b78f46e48f8e5070fbe42f1efdded429e1
marcmascarell <marcmascarell@gmail.com>
2016-08-13 22:09:44 +0200

    = fix namespace

--- src/Widget/WidgetInterface.php
+++ src/Widget/WidgetInterface.php
@@ -1,13 +1,13 @@
 <?php namespace Mascame\Artificer\Widget;
 
-use Stolz\Assets\Manager;
+use Stolz\Assets\Manager as AssetManager;
 
 interface WidgetInterface
 {
     /**
      * @return mixed
      */
-    public function assets(Manager $manager);
+    public function assets(AssetManager $manager);
 
     /**
      * @return mixed
