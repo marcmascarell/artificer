7f54451dbdb57c122baf4fdb434c992c6a0eb1a9
marcmascarell <marcmascarell@gmail.com>
2016-08-14 09:59:11 +0200

    + Added more collections

--- config/assets.php
+++ config/assets.php
@@ -5,6 +5,23 @@ return [
     'collections' => [
         // jQuery (CDN)
         'jquery-cdn' => ['//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'],
+
+        // Font Awesome (CDN)
+        'font-awesome-cdn' => [
+            '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css',
+        ],
+
+        // Bootstrap 3 CSS (CDN)
+        'bootstrap-css-cdn' => [
+            'font-awesome-cdn',
+            '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+        ],
+
+        // Bootstrap 3 JS (CDN)
+        'bootstrap-js-cdn' => [
+            'jquery-cdn',
+            '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+        ],
     ],
 
     'assets' => [
