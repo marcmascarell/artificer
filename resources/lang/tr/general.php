55002ea893bd3041fe61854c3abf85cae4eba259
Berk Gureken <berkgureken@gmail.com>
2016-10-13 01:28:25 +0300

    Added Tukish translate

---
+++ resources/lang/tr/general.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'no results' => 'sonuç yok',
+    'logout' => 'çıkış yap',
+    'filter' => 'filtre',
+    'new' => 'yeni',
+    'models' => 'modeller',
+    'dashboard' => 'pano',
+
+];
